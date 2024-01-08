75b5aff4e6a02172d2cd681c451a411612865e71
PurvaKaranjawala <purkar1993@gmail.com>
2024-01-08 20:13:57 +0530

    Rails Engine Demo

---
+++ lib/test_engine/engine.rb
@@ -0,0 +1,4 @@
+module TestEngine
+  class Engine < ::Rails::Engine
+  end
+end
---
+++ test/test_engine_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TestEngineTest < ActiveSupport::TestCase
+  test "it has a version number" do
+    assert TestEngine::VERSION
+  end
+end
